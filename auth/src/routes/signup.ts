bb472e272be05292c51281a76cca29f5883cbe89
Reculos Gerbi Neto <reculos.neto@gsw.com.br>
2021-05-24 13:50:59 -0300

    integrating with mongodb service, checking existing mail an throwing bad request error

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { BadRequestError } from "../errors/bad-request-error";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -11,16 +14,29 @@ const validator = [
     .withMessage("password must be between 4 and 20 characters"),
 ];
 
-router.post("/api/users/signup", validator, (req: Request, res: Response) => {
-  const errs = validationResult(req);
+router.post(
+  "/api/users/signup",
+  validator,
+  async (req: Request, res: Response) => {
+    const errs = validationResult(req);
 
-  if (!errs.isEmpty()) {
-    throw new Error("Invalid email or password");
+    if (!errs.isEmpty()) {
+      throw new RequestValidationError(errs.array());
+    }
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email is in use");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    return res.status(201).send(user);
   }
-
-  console.log("Creating a user...");
-
-  throw new Error("Could not connect to database");
-});
+);
 
 export { router as signUpRouter };
