bc13eaa05358aab9a2704edeb6563cce9f13fa66
Reculos Gerbi Neto <reculos.neto@gsw.com.br>
2021-05-25 20:21:09 -0300

    adding cookies session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+
 import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middlewares/error-handler";
 import { currentUserRouter } from "./routes/current-user";
@@ -10,7 +12,15 @@ import { signUpRouter } from "./routes/signup";
 
 const app = express();
 
+app.set("trust proxy", true);
+
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
