a718b8269079b063d67f3fc95e3d00a9e1b50ee5
Reculos Gerbi Neto <reculos.neto@gsw.com.br>
2021-05-27 21:42:31 -0300

    getting current user controller

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("HELOOOOOOOOO");
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const verifyed = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.send({ currentUser: verifyed });
+  } catch (err) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
