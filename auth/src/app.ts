b09abf3c7cb0050871ba7c9eda54db2d3c6cc295
Reculos Gerbi Neto <reculos.neto@gsw.com.br>
2021-05-28 19:04:57 -0300

    refactoring app to start supertest

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/error-handler";
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.use("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
