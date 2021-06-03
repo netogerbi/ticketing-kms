cffeae98f4804de2ff488f7cdb8734dc10caf3c1
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-06-02 22:09:45 -0300

    added state to signup form

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,16 +1,38 @@
+import { useState } from "react";
+
 export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <form action="">
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label htmlFor="">Email Address</label>
-        <input type="text" className="form-control" />
+        <input
+          type="text"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="form-group">
         <label htmlFor="">Password</label>
-        <input type="password" className="form-control" />
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
-      <button className="btn btn-primary">Sign Up</button>
+      <button type="submit" className="btn btn-primary">
+        Sign Up
+      </button>
     </form>
   );
 };
