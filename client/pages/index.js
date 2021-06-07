c6561d423255925110ad5791d970960c858a7aa0
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-06-07 19:21:55 -0300

    accessing auth server from client server or browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,27 @@
-const LandingPage = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    const r = await axios.get(
+      `http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser`,
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+
+    return r.data;
+  } else {
+    const r = await axios.get("/api/users/currentuser");
+
+    return r.data;
+  }
 };
 
 export default LandingPage;
