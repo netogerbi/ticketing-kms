6ed0427f776488db9b90a4bf96ae936b91ac4edb
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-06-07 19:39:28 -0300

    create default client to improve requests

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,25 +1,13 @@
-import axios from "axios";
+import client from "../api/client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    const r = await axios.get(
-      `http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser`,
-      {
-        headers: req.headers,
-      }
-    );
-
-    return r.data;
-  } else {
-    const r = await axios.get("/api/users/currentuser");
-
-    return r.data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await client(context).get("/api/users/currentuser");
+  return data;
 };
 
 export default LandingPage;
