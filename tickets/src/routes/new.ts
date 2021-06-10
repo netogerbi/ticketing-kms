4dd57ded214c3d4a71223b4a4ef4015ab970026b
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-06-10 20:12:15 -0300

    added validator to pass the validation tests

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,12 +1,22 @@
-import { currentUser, requireAuth } from "@ntgerbi/common";
+import { currentUser, requireAuth, validateRequest } from "@ntgerbi/common";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
+
+const validator = [
+  body("title").isString().trim().notEmpty().withMessage("Title must be valid"),
+  body("price")
+    .isFloat({ min: 0.01 })
+    .withMessage("Price must be a floating number"),
+];
 
 router.post(
   "/api/tickets",
   currentUser,
   requireAuth,
+  validator,
+  validateRequest,
   (req: Request, res: Response) => res.send({})
 );
 
