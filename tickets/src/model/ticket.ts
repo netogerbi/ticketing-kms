97973153280c2cf30eacc1b7095b3c7c5d11c638
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-06-11 12:16:03 -0300

    ticket model created

---
+++ tickets/src/model/ticket.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+export type TicketAttrs = {
+  title: string;
+  price: number;
+  userId: string;
+};
+
+export type TicketDocument = mongoose.Document &
+  TicketAttrs & {
+    createdAt: string;
+  };
+
+type BuildFunction = (attrs: TicketAttrs) => TicketDocument;
+
+export type TicketModel = mongoose.Model<TicketDocument> & {
+  build: BuildFunction;
+};
+
+const schema = new mongoose.Schema<TicketDocument, TicketModel>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+schema.statics.build = (attrs: TicketAttrs) => new Ticket(attrs);
+
+const Ticket = mongoose.model("Ticket", schema);
+
+export { Ticket };
