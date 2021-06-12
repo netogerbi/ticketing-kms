f1894a3d88d4824d18dbaecb6a9c40de1edddeda
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-06-12 12:19:09 -0300

    validators implementation

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -2,15 +2,26 @@ import {
   NotAuthorizedError,
   NotFoundError,
   requireAuth,
+  validateRequest,
 } from "@ntgerbi/common";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
 import { Ticket } from "../model/ticket";
 
 const router = express.Router();
 
+const validator = [
+  body("title").isString().trim().notEmpty().withMessage("Title must be valid"),
+  body("price")
+    .isFloat({ min: 0 })
+    .withMessage("Price must be a floating number and grater than 0"),
+];
+
 router.put(
   "/api/tickets/:id",
   requireAuth,
+  validator,
+  validateRequest,
   async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
 
@@ -18,7 +29,7 @@ router.put(
       throw new NotFoundError();
     }
 
-    if (ticket.userId !== req.currentUser?.id) {
+    if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
 
