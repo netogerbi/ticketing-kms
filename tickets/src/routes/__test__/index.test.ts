570d62e32f0d0a2b6d08444e1e9f387b82c21023
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-06-11 21:28:38 -0300

    test for index route created

---
+++ tickets/src/routes/__test__/index.test.ts
@@ -0,0 +1,20 @@
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+
+const createTicket = async () => {
+  await request(app).post("/api/tickets").set("Cookie", global.signup()).send({
+    title: "Test title",
+    price: 10.2,
+  });
+};
+
+it("GET: /api/tickets - 200 OK", async () => {
+  await createTicket();
+  await createTicket();
+  await createTicket();
+
+  const r = await request(app).get(`/api/tickets`).send().expect(200);
+
+  expect(r.body).toHaveLength(3);
+});
