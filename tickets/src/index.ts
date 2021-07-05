55f150f0c945b6d2d954fd962060d313cd180369
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-07-05 14:18:21 -0300

    connecting to nats with wrapper

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -10,12 +11,20 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect("ticketing", "tickets", "http://nats-srv:4222");
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log("Conected to MongoDB successfully");
+    console.log("Connected to MongoDB successfully");
   } catch (err) {
     console.error(err);
   }
