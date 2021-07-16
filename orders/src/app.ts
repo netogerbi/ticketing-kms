717c059c471fc4a9c0190364f6976d5728e66a23
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-07-15 21:00:20 -0300

    integrated routes boilerplates

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,10 +3,10 @@ import "express-async-errors";
 import cookieSession from "cookie-session";
 
 import { NotFoundError, errorHandler, currentUser } from "@ntgerbi/common";
-// import { newTicketRouter } from "./routes/new";
-// import { showTicketRouter } from "./routes/show";
-// import { indexTicketRouter } from "./routes";
-// import { updateTicketRouter } from "./routes/update";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes";
+import { deleteOrderRouter } from "./routes/delete";
 
 const app = express();
 
@@ -21,10 +21,10 @@ app.use(
 );
 
 app.use(currentUser);
-// app.use(newTicketRouter);
-// app.use(showTicketRouter);
-// app.use(indexTicketRouter);
-// app.use(updateTicketRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all("*", () => {
   throw new NotFoundError();
