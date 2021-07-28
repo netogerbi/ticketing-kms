848f26e2b5e0d4d0db89b4db9678c719e4797f72
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-07-28 13:06:56 -0300

    publishing event on cancelling order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,7 +5,9 @@ import {
   requireAuth,
 } from "@ntgerbi/common";
 import express, { Request, Response } from "express";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
 import { Order } from "../models/order";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -15,7 +17,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const orderFound = await Order.findById(orderId);
+    const orderFound = await Order.findById(orderId).populate("ticket");
 
     if (!orderFound) {
       throw new NotFoundError();
@@ -27,6 +29,13 @@ router.delete(
 
     orderFound.status = OrderStatus.Cancelled;
     await orderFound.save();
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: orderFound.id,
+      ticket: {
+        id: orderFound.ticket.id,
+      },
+    });
 
     res.status(204).send(orderFound);
   }
