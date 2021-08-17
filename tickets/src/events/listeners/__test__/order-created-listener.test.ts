df27b0306330277bb61f8f5b32207eff993c144b
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-08-17 20:35:41 -0300

    testing publish ticket updated event on listener

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -53,3 +53,11 @@ it("acks the message", async () => {
 
   expect(msg.ack).toHaveBeenCalled();
 });
+
+it("publishes a ticket updated event", async () => {
+  const { listener, data, msg, ticket } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
