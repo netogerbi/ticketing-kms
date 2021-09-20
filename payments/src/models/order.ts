6a59121ed2d5e8ce9f9c369121432766556e819c
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-09-20 20:27:59 -0300

    order model created

---
+++ payments/src/models/order.ts
@@ -0,0 +1,59 @@
+import { OrderStatus } from "@ntgerbi/common";
+import mongoose from "mongoose";
+
+type OrderAttrs = {
+  id: string;
+  version: number;
+  status: OrderStatus;
+  price: number;
+  userId: string;
+};
+
+type OrderDocument = mongoose.Document & OrderAttrs;
+
+type BuildFunction = (attrs: OrderAttrs) => OrderDocument;
+
+type OrderModel = mongoose.Model<OrderDocument> & {
+  build: BuildFunction;
+};
+
+const schema = new mongoose.Schema<OrderDocument, OrderModel>(
+  {
+    status: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+    optimisticConcurrency: true,
+    versionKey: "version",
+  }
+);
+
+schema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    price: attrs.price,
+    version: attrs.version,
+    userId: attrs.userId,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model("Order", schema);
+
+export { Order };
