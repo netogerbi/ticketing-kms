a30ab13c6a1696aeed8c2f3915c48f73c52f00ca
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-10-04 21:03:43 -0300

    adding stripe request and mocking for testing

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,9 @@ import { app } from "../../app";
 import mongoose from "mongoose";
 import { Order } from "../../models/order";
 import { OrderStatus } from "@ntgerbi/common";
+import { stripe } from "../../stripe";
+
+jest.mock("../../stripe");
 
 it("return 404 when purchasing an order that does not exist", async () => {
   await request(app)
@@ -51,4 +54,31 @@ it("return 400 when purchasing a cancelled order", async () => {
       orderId: order.id,
     })
     .expect(400);
+});
+
+it("return a 204 with valid inputs", async () => {
+  const order = await Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  }).save();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signup(order.userId))
+    .send({
+      token: "asd123",
+      orderId: order.id,
+    })
+    .expect(201);
+
+  const chargeOpts = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+
+  expect(chargeOpts).toEqual({
+    currency: "USD",
+    amount: order.price * 100,
+    source: "asd123",
+  });
 });
