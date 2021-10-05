04f07071e39054a4c77a39155d5f0632d2ca7359
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-10-05 19:53:57 -0300

    added payment model

---
+++ payments/src/models/payment.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+type PaymentAttrs = {
+  orderId: string;
+  stripeId: string;
+};
+
+type PaymentDocument = mongoose.Document & PaymentAttrs;
+
+type BuildFn = (attrs: PaymentAttrs) => PaymentDocument;
+
+type PaymentModel = mongoose.Model<PaymentDocument> & {
+  build: BuildFn;
+};
+
+const schema = new mongoose.Schema<PaymentDocument, PaymentModel>(
+  {
+    orderId: {
+      required: true,
+      type: String,
+    },
+    stripeId: {
+      required: true,
+      type: String,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+schema.static("build", (attrs: PaymentAttrs) => new Payment(attrs));
+
+const Payment = mongoose.model("Payment", schema);
+
+export { Payment };
