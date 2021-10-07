ba957c7056dc1e165c0d73bad9856bbf05a85481
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-10-07 20:49:12 -0300

    chore: added state to form and some number formatting

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,15 +1,41 @@
+import { useState } from "react";
+
 const NewTicket = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+
+    if (isNaN(value)) {
+      return;
+    }
+
+    setPrice(value.toFixed(2));
+  };
+
   return (
     <div className="">
       <h1>Create a ticket</h1>
       <form action="">
         <div className="form-group">
           <label htmlFor="">Title</label>
-          <input className="form-control" type="text" />
+          <input
+            className="form-control"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="">Price</label>
-          <input className="form-control" type="text" />
+          <input
+            className="form-control"
+            type="text"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            onBlur={onBlur}
+          />
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
