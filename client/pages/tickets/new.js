93e44e478bdab175e3ff4881c8d02a300226fe66
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-10-07 20:38:41 -0300

    added form to create new ticket

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -2,6 +2,17 @@ const NewTicket = () => {
   return (
     <div className="">
       <h1>Create a ticket</h1>
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="">Title</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Price</label>
+          <input className="form-control" type="text" />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 };
