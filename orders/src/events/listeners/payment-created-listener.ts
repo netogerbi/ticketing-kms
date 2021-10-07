260e2f7a590a47e58739df09e5b7f6f1e1a4f8c2
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-10-07 19:24:53 -0300

    listening for payment created event to update order

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,29 @@
+import {
+  Listener,
+  OrderStatus,
+  PaymentCreatedEvent,
+  Subjects,
+} from "@ntgerbi/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { queueGroupName } from "./queue-gropu-name";
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+  subject: Subjects.PaymentCreatedEvent = Subjects.PaymentCreatedEvent;
+  queueGroupName = queueGroupName;
+
+  async onMessage(
+    data: PaymentCreatedEvent["data"],
+    msg: Message
+  ): Promise<void> {
+    const order = await Order.findById(data.orderId);
+
+    if (!order) {
+      console.log("ERROR: Order not found!");
+      return msg.ack();
+    }
+
+    order.set("status", OrderStatus.Complete);
+    order.save();
+  }
+}
--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,6 +4,7 @@ import { TicketCreatedListener } from "./events/listeners/ticket-created-listene
 import { TicketUpdatedListener } from "./events/listeners/ticket-update-listener";
 import { ExpirationCompleteListener } from "./events/listeners/expiration-completed-listener";
 import { natsWrapper } from "./nats-wrapper";
+import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -38,6 +39,7 @@ const start = async () => {
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
     new ExpirationCompleteListener(natsWrapper.client).listen();
+    new PaymentCreatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
 
