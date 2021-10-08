1f16676d1d0819d0a9f21138f4c21cb8e02d5b09
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-10-08 13:13:39 -0300

    requesting tickets index

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,13 @@
 import client from "../api/client";
 
-const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
+const LandingPage = ({ currentUser, tickets }) => {
+  console.log(tickets);
   return currentUser ? <h1>Signed In</h1> : <h1>NOT Signed In</h1>;
 };
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return {};
-  // const cli = client(context);
-  // const { data } = await cli.get("/api/users/currentuser");
-  // return data;
+  const { data } = await client.get("/api/tickets");
+  return { tickets: data };
 };
 
 export default LandingPage;
