79c9e8799f51442821ab6aa6701da1a738259eae
Reculos Gerbi Neto <netogerbi2@hotmail.com>
2021-10-16 20:25:17 -0300

    chore: add orders index page and redirecting to it when order completed

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Router from "next/router";
 import StripeCheckout from "react-stripe-checkout";
 import useRequest from "../../hooks/use-request";
 
@@ -10,7 +11,7 @@ const OrderShow = ({ order, currentUser }) => {
     data: {
       orderId: order.id,
     },
-    onSuccess: (p) => console.log(p),
+    onSuccess: (p) => Router.push("/orders"),
   });
 
   useEffect(() => {
